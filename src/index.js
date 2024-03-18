1f186bf6c3939f6975cdab9603e026e0ffdd6f93
Kath <kathleensusko@gmail.com>
2024-03-18 14:57:48 -0300

    atualização segunda 18/03

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import chalk from 'chalk';
 
 const regex = '/\[([^[\]]*?)\]\((https?:\/\/[^\s?#.]*[^\s]*)\)/gm';
 
-const textoTeste='São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).';
+// const textoTeste='São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).';
 
 function extraiLinks (texto){
     const regex =/\[([^[\]]*?)\]\((https?:\/\/[^\s?#.]*[^\s]*)\)/gm;
@@ -19,10 +19,11 @@ function extraiLinks (texto){
     const resultados=capturas.map(captura=>({[captura[1]]:captura[2]}));
 
     // console.log(capturas);
-    console.log(resultados);
+    // console.log(resultados);
+    return resultados;
 }
 
-extraiLinks(textoTeste);
+// extraiLinks(texto);
 
 // console.log(chalk.blue('Olá mundo'));
 
@@ -71,19 +72,20 @@ extraiLinks(textoTeste);
 
 //-----------usando outra forma de solucionar as promessas (async/await)
 
-// async function pegaArquivo(caminhoDoArquivo){
-//     try{
-//         const encoding ="utf-8";
-//         const texto= await fs.promises.readFile(caminhoDoArquivo,encoding);
-//         console.log(chalk.cyan(texto));
-//     } catch (erro) {
-//         trataErro(erro);
-//     } finally {
-//         console.log(chalk.magenta('Operação concluída'));
-//     }
+async function pegaArquivo(caminhoDoArquivo){
+    try{
+        const encoding ="utf-8";
+        const texto= await fs.promises.readFile(caminhoDoArquivo,encoding);
+        console.log(extraiLinks(texto));
+    } catch (erro) {
+        trataErro(erro);
+    } finally {
+        console.log(chalk.magenta('Operação concluída'));
+    }
 
-// }
+}
 
-// pegaArquivo('./arquivos/texto.md');
+export default pegaArquivo;
+    // pegaArquivo('./arquivos/texto.md');
 // //pegaArquivo('./arquivos/');
 

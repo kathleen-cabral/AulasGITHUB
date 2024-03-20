d59d634ea3d1c8e59105b81f10f8b926a8953296
kath <kathleensusko@hotmail.com>
2024-03-20 14:41:44 -0300

    Atualização quarta 20/03

--- src/cli.js
+++ src/cli.js
@@ -1,31 +1,51 @@
 //Comand Line Interface (cli)
 
 import pegaArquivo from "./index.js";
-import fs from 'fs';
-import chalk from 'chalk';
 
+import fs from "fs";
 
-// 1) criar uma varialvel para recepcionar o caminho via terminal
+import chalk from "chalk";
+
+//1) criar uma variavel para recepcionar o caminho via terminal
 
 const caminho = process.argv;
-console.log(caminho);
+//console.log(caminho);
 
 // pegaArquivo(caminho[2]);
 
+function imprimeLista(resultado, identificador=""){
+    console.log(chalk.yellow('Lista de Links'),
+    chalk.black.bgGreen(identificador),
+    resultado);
+}
+
 async function processaTexto(argumentos){
-    const caminho = argumentos[2];
-    if(fs.lstatSync(caminho).isFile()){
-        const resultado =await pegaArquivo(caminho);
-        console.log(chalk.yellow('Lista de Links'),resultado);
+    const caminho=argumentos[2];
+    try{
+        fs.statSync(caminho)
     }
-    else if (fs.lstatSync(caminho).isDirectory()){
+    catch(erro){
+        if(erro.code==="ENOENT"){
+            console.log("Arquivo ou diretório não encontrado.");
+            return;
+        }
+    }
+
+    if(fs.lstatSync(caminho).isFile()){
+        const resultado = await pegaArquivo(caminho);
+        // console.log(chalk.yellow('Lista de Links'), resultado);
+        imprimeLista(resultado);
+    }else if(fs.lstatSync(caminho).isDirectory()){
         const arquivos=await fs.promises.readdir(caminho);
         arquivos.forEach(async(nomeDoArquivo)=>{
-            const lista = await pegaArquivo(`${caminho}/${nomeDoArquivo}`);
-            console.log(`${caminho}/${nomeDoArquivo}`);
-            console.log(lista);
+            const lista=await pegaArquivo(`${caminho}/${nomeDoArquivo}`);
+            //console.log(`${caminho}/${nomeDoArquivo}`);
+            //console.log(lista);
+            imprimeLista(lista,nomeDoArquivo);
         })
     }
+    
+
 }
 
 processaTexto(caminho);

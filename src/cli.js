f46f88976c199fc3b68af89a87ac44f1c1b695dc
kath <kathleensusko@gmail.com>
2024-03-21 14:53:56 -0300

    Atualização 21/03 quinta

--- src/cli.js
+++ src/cli.js
@@ -1,26 +1,33 @@
 //Comand Line Interface (cli)
 
 import pegaArquivo from "./index.js";
-
 import fs from "fs";
-
 import chalk from "chalk";
+import listaValidada from "./http-validacao.js";
 
 //1) criar uma variavel para recepcionar o caminho via terminal
 
 const caminho = process.argv;
 //console.log(caminho);
-
 // pegaArquivo(caminho[2]);
 
-function imprimeLista(resultado, identificador=""){
-    console.log(chalk.yellow('Lista de Links'),
-    chalk.black.bgGreen(identificador),
-    resultado);
+function imprimeLista(valida,resultado, identificador=""){
+    if(valida){
+        console.log(chalk.yellow('Lista de Links'),
+        chalk.black.bgGreen(identificador),
+        listaValidada(resultado));
+    }
+    else{
+        console.log(chalk.yellow('Lista de Links'),
+        chalk.black.bgGreen(identificador),
+        listaValidada(resultado));
+    }
 }
 
 async function processaTexto(argumentos){
     const caminho=argumentos[2];
+    const valida = argumentos[3]==="--valida";
+    
     try{
         fs.statSync(caminho)
     }
@@ -34,18 +41,15 @@ async function processaTexto(argumentos){
     if(fs.lstatSync(caminho).isFile()){
         const resultado = await pegaArquivo(caminho);
         // console.log(chalk.yellow('Lista de Links'), resultado);
-        imprimeLista(resultado);
+        imprimeLista(valida,resultado);
     }else if(fs.lstatSync(caminho).isDirectory()){
         const arquivos=await fs.promises.readdir(caminho);
         arquivos.forEach(async(nomeDoArquivo)=>{
             const lista=await pegaArquivo(`${caminho}/${nomeDoArquivo}`);
             //console.log(`${caminho}/${nomeDoArquivo}`);
             //console.log(lista);
-            imprimeLista(lista,nomeDoArquivo);
+            imprimeLista(valida,lista,nomeDoArquivo);
         })
     }
-    
-
 }
-
 processaTexto(caminho);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import fs from "fs";
-
 import chalk from 'chalk';
 
 const regex = '/\[([^[\]]*?)\]\((https?:\/\/[^\s?#.]*[^\s]*)\)/gm';
@@ -15,7 +14,6 @@ function extraiLinks (texto){
 
     //operador de espalhamento
     const capturas=[...texto.matchAll(regex)];
-
     const resultados=capturas.map(captura=>({[captura[1]]:captura[2]}));
 
     // console.log(capturas);
@@ -24,15 +22,10 @@ function extraiLinks (texto){
 }
 
 // extraiLinks(texto);
-
 // console.log(chalk.blue('Olá mundo'));
-
 // console.log(chalk.blue.bgWhite.bold("SENAI"));
-
 // console.log(chalk.blue('Curso',' de',' Node.JS'));
-
 // console.log(chalk.red('vermelho',chalk.underline.bgBlue('azul')));
-
 // console.log(`
 // CPU: ${chalk.red('90%')}
 // RAM: ${chalk.green('40%')}
@@ -83,10 +76,8 @@ async function pegaArquivo(caminhoDoArquivo){
     } finally {
         console.log(chalk.magenta('Operação concluída'));
     }
-
 }
 
 export default pegaArquivo;
     // pegaArquivo('./arquivos/texto.md');
 // //pegaArquivo('./arquivos/');
-
